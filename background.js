692d1305c6c1d8840e5824d810d7deecafb95ae6
pawan10 <pawandev1099@gmail.com>
2021-07-09 14:54:03 +0530

    upadate poo

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 function reddenPage(){
-    let re = /(H|h)mani (U|u)padhyay/g;
-    let element=document.querySelectorAll("p,li,h1,h2,h3,h4,h5,h6,span,div");
+    var re = /^[a-zA-Z ]{2,30}$/g;
+    var element=document.querySelectorAll("h1,p,h2,h3,h4,h5,h6");
     for(let i=0;i<element.length;i++){
         
- element[i].innerHTML = element[i].innerHTML.replace(re,"🎉🌹🌹🎉🌹🌹🎉")
+ element[i].innerHTML = element[i].innerHTML.replace(re,"💩💩")
     }
 }
 chrome.action.onClicked.addListener( (tab) => {
